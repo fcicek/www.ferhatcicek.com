924c5f51dff093bde0ced01124043f3c6c8aa1da
fcicek <86534938+fcicek@users.noreply.github.com>
2021-06-26 23:16:36 +0300

    Create sros.py

---
+++ python-nokia-sros/sros.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import re
+import math
+import sys
+
+def pop(stack):
+    try:
+        stack.pop()
+    except Exception as err:
+        print("ERROR: Unable to flush stack - %s" %err)
+
+def output(stack):
+    output = " ".join(stack)
+    print(output)
+    return output
+
+def sros_flatten(data):
+    stack = []
+    exit_detected = False
+    indent = 0
+    new_conf = ""
+
+    for line in data.lstrip().splitlines():
+        l = len(line) - len(line.lstrip())
+        nxt_indent = math.ceil(float(l/4))
+
+        if line.startswith(("#", "echo")) or line.strip() == "":
+            pass
+        elif line.strip() == "exit all":
+            new_conf = new_conf + "\n" + output(stack)
+        else:
+            if nxt_indent == 0 and line.strip() == "configure":
+                new_line = str("/") + str(line.strip())
+                stack.append(new_line)
+
+            elif nxt_indent > indent:
+                if line.strip() != "configure" and len(stack) == 0:
+                    stack.insert(0, "/configure")
+                stack.append(line.lstrip())
+
+            elif nxt_indent == indent:
+                if line.strip() != "exit":
+                    if exit_detected:
+                        stack.append(line.strip())
+                    else:
+                        if len(stack) != 0:
+                            new_conf = new_conf + "\n" + output(stack)
+                            pop(stack)
+                            stack.append(line.strip())
+                        else:
+                            stack.insert(0, "/configure")
+                            stack.append(line.strip())
+                    exit_detected = False
+
+            else:
+                if line.strip() == "exit":
+                    if not exit_detected:
+                        new_conf = new_conf + "\n" + output(stack)
+                        del stack[-2:]
+                    else:
+                        pop(stack)
+                    exit_detected = True
+
+                else:
+                    new_conf = new_conf + "\n" + output(stack)
+                    exit_detected = False
+                    pop(stack)
+            indent = nxt_indent
+    
+    return new_conf
+
+def main():
+    filename = sys.argv[1]
+    with open(filename, 'r') as f:
+        data = f.read()
+
+    sros_flatten(data)
+
+if __name__ == "__main__":
+    main()
